df4519a00fb1a5b6284a7f065c6f440c629a95b3
zhuravel17 <zhuravlevakat1996@gmail.com>
2025-04-12 19:46:03 +0400

    feat: add getCurrentFunctionName and getFunctionBody

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -18,7 +18,7 @@
  *
  */
 function getCurrentFunctionName() {
-  throw new Error('Not implemented');
+  return getCurrentFunctionName.name;
 }
 
 /**
@@ -32,8 +32,8 @@ function getCurrentFunctionName() {
  *   getFunctionBody(hiHello) => "function hiHello() { console.log('hello world'); }"
  *
  */
-function getFunctionBody(/* func */) {
-  throw new Error('Not implemented');
+function getFunctionBody(func) {
+  return func ?? '';
 }
 
 /**
