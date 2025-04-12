76dc9c412afdfdda19bc73672413e931b3b877d3
zhuravel17 <zhuravlevakat1996@gmail.com>
2025-04-12 19:52:46 +0400

    feat: add getArgumentsCount

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -50,8 +50,8 @@ function getFunctionBody(func) {
  *  ]) => [0, 1, 2]
  *
  */
-function getArgumentsCount(/* funcs */) {
-  throw new Error('Not implemented');
+function getArgumentsCount(funcs) {
+  return funcs.map((func) => func.length);
 }
 
 /**
